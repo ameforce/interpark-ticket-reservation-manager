6d08cbcd70dd662254fd050b0914339242a7c564
jongin.kim <enmsoftware@gmail.com>
2023-05-15 13:01:29 +0900

    Initial Commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # interpark-ticket-reservation-manager
+### It's a project that helps you make automatic reservations at Interpark tickets.
---
+++ main.py
@@ -0,0 +1,10 @@
+from libs.selechecker import selechecker
+
+
+def main():
+    selechecker.driver_check()
+    return
+
+
+if __name__ == '__main__':
+    main()
